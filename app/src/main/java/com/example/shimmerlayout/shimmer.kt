56322a5a2d56581a88b0d1133037c442ffd72f01
Felipe <felipe2204sv@gmail.com>
2022-10-27 18:44:09 -0300

    create shimmer handler

---
+++ app/src/main/java/com/example/shimmerlayout/shimmer.kt
@@ -0,0 +1,41 @@
+package com.example.shimmerlayout
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+fun Modifier.shimmer(
+    shimmerColors: List<Color> = listOf(
+        Color.LightGray.copy(alpha = 0.6f),
+        Color.LightGray.copy(alpha = 0.2f),
+        Color.LightGray.copy(alpha = 0.6f),
+    ),
+    finalOffset: Float = 1000f,
+    duration: Int = 1000
+): Modifier = composed {
+
+    val transition = rememberInfiniteTransition()
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = finalOffset,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = duration,
+                easing = FastOutSlowInEasing
+            )
+        )
+    )
+
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
+    this.then(Modifier.background(brush))
+}
