1079242bd082cffdf0841d902ec6f6d7c10e6e18
Felipe <felipe2204sv@gmail.com>
2022-10-27 17:47:26 -0300

    finish skeleton animation

---
+++ app/src/main/java/com/example/shimmerlayout/AnimatedShimmer.kt
@@ -0,0 +1,66 @@
+package com.example.shimmerlayout
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AnimatedShimmer() {
+    val shimmerColors = listOf(
+        Color.LightGray.copy(alpha = 0.6f),
+        Color.LightGray.copy(alpha = 0.2f),
+        Color.LightGray.copy(alpha = 0.6f),
+    )
+
+    val transition = rememberInfiniteTransition()
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing
+            )
+        )
+    )
+
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
+    ShimmerGridItem(brush = brush)
+}
+
+@Composable
+fun ShimmerGridItem(brush: Brush) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 10.dp)
+    ) {
+        Spacer(modifier = Modifier
+            .size(200.dp)
+            .clip(CircleShape)
+            .background(brush))
+    }
+}
+
+@Preview
+@Composable
+fun Aoba() {
+    AnimatedShimmer()
+}
--- app/src/main/java/com/example/shimmerlayout/MainActivity.kt
+++ app/src/main/java/com/example/shimmerlayout/MainActivity.kt
@@ -17,12 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShimmerLayoutTheme {
-                // A surfacaae container using the 'background' color from the theme
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                   AnimatedShimmer()
                 }
             }
         }
